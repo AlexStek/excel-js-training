28ccf6c63662e6990b8566ef56054933885faf02
AleksStek <aleksstek@yandex.ru>
2023-06-30 17:39:25 +0700

    add webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
 module.exports = {
@@ -5,7 +9,30 @@ module.exports = {
   mode: 'development',
   entry: './index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].bundle.[hash].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { 
+          from: path.resolve(__dirname, 'src/favicon.ico'), 
+          to:  path.resolve(__dirname, 'dist')
+        }
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].bundle.[hash].css'
+    })
+  ],
 }
